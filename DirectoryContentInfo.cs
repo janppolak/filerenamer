929699e9d4afb7959efcd6c1b871896a461a3a4c
Jan Polak <jan.p.polak@gmail.com>
2021-04-23 21:22:44 +0200

    Program wyświetla nazwy plików znadujących się w folderze ze zdjęciami

---
+++ DirectoryContentInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileNameEditor
+{
+    class DirectoryContentInfo
+    {
+        private const string _path = @"c:\users\janpp\desktop\renamealbum";
+
+
+        public void ShowDirectoryContent()
+        {
+            string[] fileEntries = Directory.GetFiles(_path);
+            foreach (var item in fileEntries)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileNameEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryContentInfo directory = new DirectoryContentInfo();
+            directory.ShowDirectoryContent();
+        }
+    }
+}
