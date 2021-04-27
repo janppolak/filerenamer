78726058c296ff97beb9a2812b2708c8e6d55a15
Jan Polak <jan.p.polak@gmail.com>
2021-04-27 21:40:13 +0200

    Ostatni update przed spotkaniem z Mikołajem

--- DirectoryContentInfo.cs
+++ DirectoryContentInfo.cs
@@ -7,22 +7,24 @@ namespace FileNameEditor
 {
     class DirectoryContentInfo
     {
-        private const string _path = @"c:\users\janpp\desktop\renamealbum";
-        
+        //private const string _path = @"c:\users\janpp\desktop\renamealbum";
+        string _path = Directory.GetCurrentDirectory();
 
         public void ShowDirectoryContent()
         {
             string[] fileEntries = Directory.GetFiles(_path);
+
             foreach (var fileName in fileEntries)
             {  
-                DateTime[] dateTimes = { Directory.GetCreationTime(_path) };
+                DateTime[] dateTimes = { File.GetCreationTime(fileName) };
                 foreach (var fileDate in dateTimes)
                 {
                     Console.WriteLine(fileName + "||" + fileDate);
                 }
             }
 
-            
+            //collection1.Concat(collection2); 
+            //collection1.select(x => (baseType)x).Concat(collection2.select(x => (baseType)x));
 
         }
     }
