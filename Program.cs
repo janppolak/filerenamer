d07ee03d90ada72bb53d8eef42bd120f6efc19ca
Jan Polak <jan.p.polak@gmail.com>
2021-04-28 19:38:10 +0200

    initial commit

--- Program.cs
+++ Program.cs
@@ -9,9 +9,24 @@ namespace FileNameEditor
             const string path = @"c:\users\janpp\desktop\renamealbum";
             //string path = Directory.GetCurrentDirectory();
 
+            var hasPermission = UserHasPermissionToModifyFileNames();
+
+            if(hasPermission == false)
+            {
+                Console.WriteLine("You don't have permission. Press any key to exit");
+                Console.ReadKey();
+                return;
+            }
+
             var fileRenamer = new FileRenamer(path);
             var fileDtos = fileRenamer.GetFileDtosFromDirectory();
             fileRenamer.RenameFiles(fileDtos);
+        }
+
+        private static bool UserHasPermissionToModifyFileNames()
+        {
+            // TODO: Implement in the future
+            return false;
         }
     }
 }
